5186deeaba108cf25c7e7e697ced1cfe71a45c7b
sy <shiyung@gmail.com>
2024-09-18 16:50:07 +0800

    config: personal neovim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local o = vim.opt
+
+-- My options
+
+o.number = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.expandtab = false
